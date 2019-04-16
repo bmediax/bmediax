2538faf55cb697354c7dc2e39c832beeffe83141
Brian Mediax <bmediax@icloud.com>
2019-04-15 18:38:16 -0700

    Slider WORKS MUTHA FUCKAAAA

--- js/slider.js
+++ js/slider.js
@@ -1,99 +1,15 @@
-/*$(document).ready(function() {
-   $('.project-slider').slick({
-   // centerMode: true,
-   variableWidth: true,
-   slidesToShow: 2,
-   slidesToScroll: 1,
-   // centerMode: true,
-   // infinite: true,
-   autoplay: true,
-   autoplaySpeed: 2000,
-   speed: 300,
-   dots: true,
-   arrows: true,
-   // fade: true,
-   pauseOnHover: true,
-   // infinite: true,
-   responsive: [
-  {
-    breakpoint: 600,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  },
-  {
-    breakpoint: 480,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }
-   ]
-      // slidesToShow: 1,
-      // adaptiveHeight: true
-   });
-   $('.project-slider-full').slick({
+// // jQuery
+// var $carousel = $('.carousel').flickity({
+//   fullscreen: true,
+// });
 
-     cssEase: 'linear',
-     centerMode: true,
-     variableWidth: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.project-slider-nav',
-      responsive: [{
-         breakpoint: 1024,
-         settings: {
-             arrows: false,
-             centerMode: false,
-             centerPadding: '40px',
-             variableWidth: false,
-             slidesToShow: 1
-         },
-         breakpoint: 1750,
-         settings: {
-             arrows: false,
-             centerMode: false,
-             centerPadding: '40px',
-             variableWidth: false,
-             slidesToShow: 1
-         }
-     }],
-   });
-   $('.project-slider-nav').slick({
-      centerMode: true,
-      centerPadding: '70px',
-      autoplay: true,
-      autoplaySpeed: 2000,
-      speed: 300,
-      pauseOnHover: true,
-      arrows: true,
-      // variableWidth: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      asNavFor: '.project-slider-full',
-      dots: true,
-      focusOnSelect: true,
-      responsive: [
-         {
-            breakpoint: 600,
-            settings: {
-               slidesToShow: 2,
-               slidesToScroll: 1,
-               centerPadding: '30px',
-            }
-         },
-         {
-            breakpoint: 480,
-            settings: {
-               slidesToShow: 1,
-               slidesToScroll: 1,
-               centerPadding: '45px',
-            }
-         }
-      ]
-   });
+// 1st carousel, main
+$('.carousel-main').flickity({
+   fullscreen: true,
+   contain: true,
+   wrapAround: true,
+   adaptiveHeight: true,
+   selectedAttraction: 0.01,
+   friction: 0.15,
+   imagesLoaded: true
 });
-*/
