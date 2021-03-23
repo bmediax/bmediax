5a4d230d483d3390206e87aec7ad045a3747c0a4
bmediax <contact@bmediax.com>
2021-03-22 17:15:20 -0700

    Additional Animation Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Helmet } from 'react-helmet'
+import { RiLinksLine } from 'react-icons/ri'
 import '../styles/comingsoon.scss'
 
 // markup
@@ -12,7 +13,7 @@ const IndexPage = () => {
       <div className="container">
         <div className="wrapper">
           <h1> Website Under Construction </h1>
-          <a href="https://bmediax.com"> View Portfolio </a>
+          <a href="https://bmediax.com"> View Portfolio <RiLinksLine /></a>
         </div>
       </div>
     </div>
