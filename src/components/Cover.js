741153040eb601ca49bf6c2c46e4d0756e5baf0a
Bmediax <contact@bmediax.com>
2021-04-27 10:51:19 -0700

    Dark Mode Button Ghost Iteration

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -8,7 +8,7 @@ import * as homepageStyles from '../styles/homepage.module.scss'
 const Cover = () => {
     return (
         <div className={homepageStyles.cover}>
-            <StaticImage src="../images/profile/headshot.jpg" alt="Bmediax Headshot" width={150} placeholder="blurred" className="coverheadshot" />
+            <StaticImage src="../images/profile/headshot.png" alt="Bmediax Headshot" width={150} placeholder="blurred" className="coverheadshot" />
             <div className={homepageStyles.text}>
                 <h1>Hello there, my name is Brian!</h1>
                 <p>I am a <span className="azure">Digital Designer</span> and <span className="azure">Web Developer</span> with over <span className="azure">7+ years</span> of experience <span className="azure">designing, prototyping, developing, and deploying</span> large scale pixel-perfect <span className="azure">websites and web apps</span></p>
