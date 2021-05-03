ae85e531b5e0526ef39ca53870fd7aa9e598dc50
Bmediax <contact@bmediax.com>
2021-05-03 15:20:59 -0700

    Added New Section Friends Section

---
+++ src/components/FriendsSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendsSection = () => {
+    return (
+        <>
+            Friends Section   
+        </>
+    );
+};
+
+export default FriendsSection;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Layout from '../layout/index.js'
 import Cover from '../components/Cover'
 import RecentProjectsSection from '../components/RecentProjectsSection.js'
-import AwardsSection from '../components/AwardsSection.js'
+import FriendsSection from '../components/FriendsSection.js'
+// import AwardsSection from '../components/AwardsSection.js'
 
 const homepage = () => {
     return (
         <Layout title="Homepage">
             <Cover />
             <RecentProjectsSection />
-            <AwardsSection />
+            {/* <AwardsSection /> */}
+            <FriendsSection />
         </Layout>
     )
 }
