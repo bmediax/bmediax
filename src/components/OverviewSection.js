c65d447c87a7f6c2e912260706f0dbe1d45d3cc8
bmediax <contact@bmediax.com>
2021-05-04 18:36:38 -0700

    Side Bar and Overview Almost Finished

--- src/components/OverviewSection.js
+++ src/components/OverviewSection.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import * as portfolioStyles from '../styles/portfolio.module.scss'
 import SectionPortfolioLayout from '../layout/SectionPortfolioLayout';
+import SideBar from './modules/SideBar'
 
 const OverviewSection = () => {
     return (
         <SectionPortfolioLayout idLabel="overview" title="Overview" icon="Check Mark" bgLightDark align>
-            <div className="column-layout">
-                <div className="two-column_2-1">2</div>
-                <div className="two-column_1-2">1</div>
+            <div className={portfolioStyles.columnLayout}>
+                <div className={portfolioStyles.twoColumn_2to1}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultricies dignissim elit, ac lobortis leo fermentum pulvinar. Fusce quis hendrerit mauris, ac elementum urna. Sed vel ex vel leo fermentum ultricies et eu dolor. Aliquam id justo id nunc sagittis tincidunt nec non ante. Vestibulum auctor eget ex cursust incidunt nec non ante. Vestibulum.</p>
+                </div>
+                <div className={portfolioStyles.twoColumn_1to2}>
+                    <SideBar />
+                </div>
             </div>
         </SectionPortfolioLayout>
     );
---
+++ src/components/modules/SideBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as SideBarStyles from './SideBar.module.scss'
+
+const SideBar = () => {
+    return (
+        <div className={SideBarStyles.wrapper}>
+            {/* Role */}
+            <div className={SideBarStyles.section}>
+                <h3>Role</h3>
+                <ul>
+                    <li>Web Designer</li>
+                </ul>
+            </div>
+            <div className={SideBarStyles.section}>
+                {/* Work */}
+                <h3>Work</h3>
+                <ul>
+                    <li>Web Development</li>
+                </ul>
+            </div>
+
+            <div className={SideBarStyles.section}>
+                {/* Deliverable */}
+                <h3>Deliveriable</h3>
+                <ul>
+                    <li>Gatsby SPA Website</li>
+                    <li>Headless CMS Integration</li>
+                </ul>
+            </div>
+
+            <div className={SideBarStyles.section}>
+                {/* Deliverable */}
+                <button class="btn primary fullwidth"> View Project </button>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
