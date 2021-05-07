8ce605c92cd0b1c1f4e2bc9792ba999677a768df
Bmediax <contact@bmediax.com>
2021-05-07 15:37:39 -0700

    Cleaned Code

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -15,8 +15,6 @@ const Cover = ({ coverData }) => {
             <StaticImage src="../images/profile/headshot.png" alt="Bmediax Headshot" width={150} placeholder="blurred" className="coverheadshot" />
             <div className={homepageStyles.text}>
                 <h1>{ coverData.title.text }</h1>
-                {/* <p>I am a <span className="azure">Digital Designer</span> and <span className="azure">Web Developer</span> with over <span className="azure">7+ years</span> of experience <span className="azure">designing, prototyping, developing, and deploying</span> large scale pixel-perfect <span className="azure">websites and web applications</span></p> */}
-                {/* <p>{coverData.description.text}</p> */}
                 <RichText render={coverData.description.raw} />
                 <div className={homepageStyles.buttonWrapper}>
                     <DropdownModule />
