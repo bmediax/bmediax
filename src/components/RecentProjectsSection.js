e135fa5c33bfa3e66c201d148e8f1f7dc7f8e3d9
Bmediax <contact@bmediax.com>
2021-05-07 16:49:27 -0700

    Card information Coming through from prismic

--- src/components/RecentProjectsSection.js
+++ src/components/RecentProjectsSection.js
@@ -1,15 +1,63 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby'
 import SectionLayout from '../layout/SectionLayout';
 import * as styles from '../styles/homepage.module.scss'
 import CTACardModule from './modules/CTACardModule';
-import { portfolioData } from '../data/portfolioData'
+// import { portfolioData } from '../data/portfolioData'
 
 const RecentProjectsSection = () => {
+    const data = useStaticQuery(graphql`
+    query recentProjectsQuery($filter: PrismicPortfolioFilterInput = {uid: {in: ["chicanxshop", "reikoforpcc", "edutainment"]}}) {
+        allPrismicPortfolio(filter: $filter) {
+            edges {
+                node {
+                    data {
+                        cover_title {
+                            title {
+                                text
+                            }
+                            client {
+                                text
+                            }
+                            background_image {
+                                localFile {
+                                    childImageSharp {
+                                        gatsbyImageData(width: 350, placeholder: DOMINANT_COLOR)
+                                    }
+                                }
+                            }
+                        }
+                        overview {
+                            description {
+                                text
+                            }
+                            role {
+                                text
+                            }
+                            work {
+                                text
+                            }
+                        }
+                    }
+                    uid
+                }
+            }
+        }
+    }
+`)
+    const recentProjectData = data.allPrismicPortfolio.edges
+    console.log(recentProjectData)
     return (
         <SectionLayout label={styles.recentProjects} title="Recent Projects" subtitle="Portfolio">
             <div className={styles.cardWrapper}>
-                {portfolioData.slice(0, 3).map((portfolio, index) => (
-                    <CTACardModule {...portfolio} key={index} />
+                {recentProjectData.map((portfolio, index) => (
+                    <CTACardModule key={index} 
+                        title={portfolio.node.data.cover_title[0].client.text} 
+                        raw={portfolio.node.data.cover_title[0].client.text} 
+                        cover={portfolio.node.data.cover_title[0].background_image.localFile}
+                        url={portfolio.node.uid}
+                        work={portfolio.node.data.overview[0].work.text}
+                        shortDescr={portfolio.node.data.overview[0].description.text} />
                 ))}
             </div>
         </SectionLayout>
--- src/components/modules/CTACardModule.js
+++ src/components/modules/CTACardModule.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as CTACardStyles from './CTACard.module.scss'
 // import * as variables from '../../styles/_variables.module.scss'
 
 const CTACardModule = ({ title, raw, cover, url, work, shortDescr }) => {
+    const image = getImage(cover)
     return (
         <div className={CTACardStyles.CTACard}>
-            <img src={cover} alt={`${raw} Cover`} width="100%" className={CTACardStyles.coverImage} />
+            <GatsbyImage image={image} alt={`${raw} Cover`} width="100%" className={CTACardStyles.coverImage} />
             <div className={CTACardStyles.text}>
                 <span className={CTACardStyles.headerText}>
                     <h5>{work}</h5>
                     <h3>{title}</h3>
                 </span>
                 <p>{shortDescr}</p>
-                <a href={url} className="btn primary">Learn More</a>
+                <a href={`/portfolio/${url}`} className="btn primary">Learn More</a>
             </div>
         </div>
     );
