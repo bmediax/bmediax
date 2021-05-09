b4472e5aa01fd6b6cd69b19e6f39f3d896a2007c
bmediax <contact@bmediax.com>
2021-05-09 13:56:29 -0700

    Added Bmediax btn to coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet'
 import { RiLinksLine } from 'react-icons/ri'
 import { AiOutlineCloudDownload } from 'react-icons/ai'
 import '../styles/comingsoon.scss'
+import DropdownModule from '../components/modules/dropdownModule';
 import bmediaxResume from '../assets/files/Bmediax_Resume.pdf'
 
 // markup
@@ -17,9 +18,9 @@ const IndexPage = () => {
           <h4 style={{ textAlign: "center", marginBottom:"10px" }}>Coming Soon...</h4>
           <h1> Website Under Construction </h1>
           <div className="links-media">
-            <a href="https://bmediax.com" className="btn primary activePrimary"> View Portfolio <RiLinksLine /></a>
+            <DropdownModule />
+            <a href="https://bmediax.com" className="btn ghost"> View Portfolio <RiLinksLine /></a>
             <a href={bmediaxResume} download="Bmediax_Resume" className="btn ghost"> Download Resume <AiOutlineCloudDownload /></a>
-            {/* <a href={bmediaxPortfolio} download="Bmediax_Resume"> Download Résumé <AiOutlineCloudDownload /></a> */}
           </div>
         </div>
       </div>
