1fe794485d00d33532bbd2755da0f97fc69a8494
bmediax <contact@bmediax.com>
2021-05-12 21:37:53 -0700

    Updates to Portfolio Page

--- src/layout/portfolioTemplate.js
+++ src/layout/portfolioTemplate.js
@@ -5,6 +5,7 @@ import ImageSection from '../components/ImageSection';
 import LegionBar from '../components/modules/LegionBar';
 import NextProjectSection from '../components/NextProjectSection';
 import OverviewSection from '../components/OverviewSection';
+import PortfolioTitle from '../components/PortfolioTitle';
 import ResultsSection from '../components/ResultsSection';
 import Layout from '../layout'
 // import * as portfolioStyles from '../styles/portfolio.module.scss'
@@ -14,8 +15,9 @@ const PortfolioTemplate = ({ data, pageTitle }) => {
         <>
         <Layout title={pageTitle}>
             <>
-                <CoverTitle coverData={data.cover_title[0]} />
+                <PortfolioTitle coverData={data.cover_title[0]}/>
                 <LegionBar />
+                <CoverTitle coverData={data.cover_title[0]} />
                 <OverviewSection overviewData={data.overview[0]} projectLinks={data.project_links} />
                 <ImageSection image={data.design_image} />
                 <DesignSection {...data} />
