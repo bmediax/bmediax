492898f00d495227bde047ecf2608f48fa66691d
Bmediax <contact@bmediax.com>
2021-05-18 10:47:12 -0700

    Added 404 Custom Page

--- src/layout/SectionPageLayout.js
+++ src/layout/SectionPageLayout.js
@@ -4,7 +4,7 @@ import { titleTextFadeIn } from '../data/motionVariants'
 // import * as portfolioStyles from '../styles/portfolio.module.scss'
 import { CheckMarkIcon, PaintIcon } from '../components/Icons'
 
-const SectionPageLayout = ({ title, isDark, bgLightDark, icon, children, label, idLabel,  bgColor, align, sectionMaxWidth }) => {
+const SectionPageLayout = ({ title, isDark, bgLightDark, icon, children, label, idLabel,  bgColor, align, sectionMaxWidth, bigTitle}) => {
     const checkIcon = (icn) => {
         if ( icn === "Check Mark" ) {
             return <CheckMarkIcon />
@@ -18,7 +18,7 @@ const SectionPageLayout = ({ title, isDark, bgLightDark, icon, children, label, 
     return (
         <section id={idLabel} className={label} style={{ backgroundColor: bgColor }}>
             <div className={`section-title ${isDark ? 'darkTitle' : '' } sectionMaxWidth ${sectionMaxWidth ? 'sectionMaxWidth' : ''}`}>
-                <motion.h2 initial="hidden" animate="show" exit="exit" variants={titleTextFadeIn} style={{ textAlign :  align ? 'left' : 'center'}}>
+                <motion.h2 initial="hidden" animate="show" exit="exit" variants={titleTextFadeIn} style={{ textAlign :  align ? 'left' : 'center', fontSize : bigTitle ? 'clamp(5rem, 15vw, 20rem)' : ''}}>
                     {title} <span className={`sectionPortfolioIcon ${bgLightDark ? 'lightdark' : ''}`}>{checkIcon(icon)}</span> 
                 </motion.h2>
             </div>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,38 +1,22 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
+import { BiArrowBack } from 'react-icons/bi'
+import SectionPageLayout from '../layout/SectionPageLayout';
+import Layout from '../layout/index.js'
+import * as notfoundStyles from '../styles/404.module.scss'
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout title="404 - Not Found">
+      <SectionPageLayout label={notfoundStyles.fourofour} title="404" bigTitle>
+        <div className={notfoundStyles.wrapper}>
+          <p>We couldn't find what you were looking for :(</p>
+          <Link to={'/'} className="btn ghost">
+            <BiArrowBack />Back to Home</Link>
+        </div>
+      </SectionPageLayout>
+    </Layout>
   )
 }
 
