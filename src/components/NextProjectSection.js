6b6afb6c512e34ed9322c3f7a94e2ac3ddfd931d
Bmediax <contact@bmediax.com>
2021-05-19 10:00:29 -0700

    Added a few more buttons to the bottom for navigational purposes

--- src/components/NextProjectSection.js
+++ src/components/NextProjectSection.js
@@ -5,13 +5,22 @@ import * as portfolioStyles from '../styles/portfolio.module.scss'
 
 const NextProjectSection = ({ nextProjectData }) => {
     return (
-        <div className={portfolioStyles.SCA_container}>
-            <div className={portfolioStyles.SCA_wrapper}>
-                <div>View my Next Project!</div>
-                <Link to={`/portfolio/${nextProjectData.uid}`} 
-                    className="btn outline-active fullwidth-btn" style={{ width: "100%" }}>
-                        Next Project: <span style={{ textTransform: 'capitalize' }}>{nextProjectData.uid}</span> <BsArrowRight />
-                </Link>
+        <div style={{padding: "150px 5%"}}>
+            <div className={portfolioStyles.SCA_container}>
+                <div className={portfolioStyles.SCA_wrapper}>
+                    <div>View my Next Project!</div>
+                    <Link to={`/portfolio/${nextProjectData.uid}`} 
+                        className="btn outline-active fullwidth-btn" style={{ width: "100%" }}>
+                            Next Project: <span style={{ textTransform: 'capitalize' }}>{nextProjectData.uid}</span> <BsArrowRight />
+                    </Link>
+                </div>
+            </div>
+            <h1 className={portfolioStyles.message}>Or</h1>
+            <div className="pageColumnLayout sectionMaxWidth">
+                <div className="cardLink_wrapper">
+                    <h1>View my Work</h1>
+                    <Link to="/work" className="btn outline-active">View Work</Link>
+                </div>
             </div>
         </div>
     );
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { motion } from 'framer-motion'
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
+import { HiArrowCircleDown } from 'react-icons/hi';
 import SectionPageLayout from '../layout/SectionPageLayout';
 import Layout from '../layout/index.js'
 import WorkImage from '../components/workImage';
 import TagFilter from '../components/modules/TagFilter';
 import { TagContext } from '../contexts/TagContext'
+import bmediaxResume from '../assets/files/Bmediax_Resume.pdf'
 
 const Work = ({ data }) => {
   const [ tag, setTag ] = useState({
@@ -44,6 +46,19 @@ const Work = ({ data }) => {
                               ))}
                           </Masonry>
                       </ResponsiveMasonry>
+                  </div>
+              </div>
+          </SectionPageLayout>
+          <SectionPageLayout idLabel="meetBrian">
+              <div className="pageColumnLayout sectionMaxWidth" style={{ gridGap: "2em"}}>
+                  <div className="cardLink_wrapper" style={{ flex: "1" }}>
+                      <h1>View Resume</h1>
+                      {/* <a href={bmediaxResume} download="Bmediax_Resume" className="btn primary clickDownloadResume">Download Resume <HiArrowCircleDown style={{ position: 'relative', top: '2px' }}/></a> */}
+                      <a href={bmediaxResume} download="Bmediax_Resume" className="btn outline-active">Download Resume</a>
+                  </div>
+                  <div className="cardLink_wrapper" style={{ flex: "1" }}>
+                      <h1>View Portfolio</h1>
+                      <Link to="/portfolio/edutainment" className="btn outline-active clickViewPortfolioFromAbout">View a Case Study</Link>
                   </div>
               </div>
           </SectionPageLayout>
