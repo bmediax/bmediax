f5ac598c6879a9f2f2e9cdd2c45455fcd4fdbca6
bmediax <contact@bmediax.com>
2021-05-23 13:27:39 -0700

    Created BensonHS Portfolio Item

---
+++ src/pages/portfolio/bensonhs2016.js
@@ -0,0 +1,211 @@
+import React from 'react';
+import { graphql } from 'gatsby'
+import PortfolioTemplate from '../../layout/PortfolioTemplate'
+
+const bensonhs2016 = ({ data }) => {
+    if (!data) return null
+    const document = data.allPrismicPortfolio.edges[0].node.data
+    return (
+        <>
+          <PortfolioTemplate data={document} pageTitle="Benson H.S 2016" />
+        </>
+    );
+};
+
+export default bensonhs2016;
+
+export const query = graphql`
+query bensonhs2016PortfolioQuery {
+  allPrismicPortfolio(filter: {uid: {eq: "bensonhs2016"}}) {
+    edges {
+      node {
+        data {
+          cover_title {
+            client {
+              text
+            }
+            date {
+              text
+            }
+            title {
+              text
+            }
+            background_image {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    width: 2000
+                    placeholder: DOMINANT_COLOR
+                    webpOptions: {quality: 100}
+                  )
+                }
+              }
+              alt
+            }
+          }
+          design {
+            description {
+              text
+            }
+            quote {
+              text
+            }
+          }
+          design_image {
+            alt
+            localFile {
+              childImageSharp {
+                gatsbyImageData(
+                  height: 550
+                  placeholder: DOMINANT_COLOR
+                  webpOptions: {quality: 100}
+                )
+              }
+            }
+          }
+          next_project {
+            uid
+          }
+          overview {
+            deliverables {
+              text
+              raw
+              html
+            }
+            description {
+              text
+              raw
+              html
+            }
+            problem {
+              raw
+            }
+            solution {
+              raw
+            }
+            project_link {
+              url
+            }
+            role {
+              text
+              raw
+              html
+            }
+            work {
+              text
+              raw
+              html
+            }
+            design_tools {
+              text
+              raw
+              html
+            }
+            development_tools {
+              text
+              raw
+              html
+            }
+          }
+          project_links {
+            label {
+              text
+            }
+            link {
+              url
+            }
+          }
+          results {
+            description {
+              text
+              raw
+            }
+          }
+          results_image {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(
+                  height: 550
+                  placeholder: DOMINANT_COLOR
+                  webpOptions: {quality: 100}
+                )
+              }
+            }
+            alt
+          }
+          body {
+            ... on PrismicPortfolioBodyDesignColorPalette {
+              slice_type
+              items {
+                colour
+              }
+            }
+            ... on PrismicPortfolioBodyDesignImageGallery {
+              slice_type
+              items {
+                image {
+                  alt
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData (
+                        width:2000
+                        placeholder: DOMINANT_COLOR
+                        webpOptions: {quality: 100}
+                      )
+                    }
+                  }
+                }
+              }
+              primary {
+                title {
+                  text
+                }
+                column
+              }
+            }
+            ... on PrismicPortfolioBodyDesignVideo {
+              slice_type
+              primary {
+                column
+                title {
+                  text
+                }
+              }
+              items {
+                video_url {
+                  url
+                }
+                video_height {
+                  text
+                }
+              }
+            }
+            ... on PrismicPortfolioBodyResultsImageGallery {
+              slice_type
+              items {
+                image {
+                  alt
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData (
+                        width:2000
+                        placeholder: DOMINANT_COLOR
+                        webpOptions: {quality: 100}
+                      )
+                    }
+                  }
+                }
+              }
+              primary {
+                title {
+                  text
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
