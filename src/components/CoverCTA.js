103a75abf1053f5d6257d88ebc0588b15bf541a5
bmediax <contact@bmediax.com>
2021-07-02 13:53:25 -0700

    Added Cover CTA Initial

---
+++ src/components/CoverCTA.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import { RichText } from 'prismic-reactjs'
+// import { StaticImage } from 'gatsby-plugin-image'
+
+// External
+// import { RiInboxFill } from 'react-icons/ri'
+// import { Link } from 'gatsby'
+import * as homepageStyles from '../styles/homepage.module.scss'
+// import DropdownModule from './modules/dropdownModule';
+
+// Icons
+// import { EDIcon, AztecEye, Eye, Bone } from '../components/Icons'
+
+const CoverCTA = () => {
+    return (
+        <div className={homepageStyles.coverCTA}>
+            Cover CTA
+        </div>
+    );
+};
+
+export default CoverCTA;
