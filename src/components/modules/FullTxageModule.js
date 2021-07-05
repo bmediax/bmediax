8783fd6b8a303a17dfa0ec67c8cd1292de20414b
bmediax <contact@bmediax.com>
2021-07-05 14:52:37 -0700

    - Changed Txage Component Styling

--- src/components/modules/FullTxageModule.js
+++ src/components/modules/FullTxageModule.js
@@ -13,19 +13,17 @@ const FullTxageModule = () => {
                     alt="A Placeholder Image"
                     src="https://images.unsplash.com/photo-1519989041117-c6bf935facde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80" 
                     loading="lazy"
-                    className="txageImage"
-                    height={300} />
-                <div className="txageText_wrapper">
-                    <div className="txageText">
-                        <h4>Web Development & Design</h4>
-                        <h3>Reiko for PCC</h3>
-                        <p>Lorem Ipsum dolor sit amet, consecetur adipiscing elit, sed eiusmod tempor...</p>
-                        <Link to="/portfolio">
-                            <button className="btn outline-inverse">
-                                View Case Study
-                            </button>
-                        </Link>
-                    </div>
+                    className={txageStyles.txageImage}
+                />
+                <div className={txageStyles.txageText}>
+                    <h4>Web Development & Design</h4>
+                    <h3>Reiko for PCC</h3>
+                    <p>Lorem Ipsum dolor sit amet, consecetur adipiscing elit, sed eiusmod tempor...</p>
+                    <Link to="/portfolio">
+                        <button className="btn outline-inverse">
+                            View Case Study
+                        </button>
+                    </Link>
                 </div>
             </div>
         </div>
