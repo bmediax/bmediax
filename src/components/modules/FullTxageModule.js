b9a73baed793d65fe43487772b5e2d1f49140b7a
Brian Moreno <bmoreno@Brian-Morenos-MBP.local>
2021-07-06 22:43:45 -0700

    Txage Responive

--- src/components/modules/FullTxageModule.js
+++ src/components/modules/FullTxageModule.js
@@ -8,12 +8,6 @@ const FullTxageModule = ({ title, raw, cover, url, work, shortDescr }) => {
     return (
         <div className={txageStyles.txageContainer}>
             <div className={txageStyles.fullTxage}>
-                {/* <StaticImage 
-                    alt="A Placeholder Image"
-                    src="https://images.unsplash.com/photo-1519989041117-c6bf935facde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80" 
-                    loading="lazy"
-                    className={txageStyles.txageImage}
-                /> */}
                 <GatsbyImage 
                     image={image} 
                     alt={`${raw} Cover`} 
