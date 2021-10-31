dfe55e8af2ef3cd09c613470fe4b00d2a7cc23da
Bmediax <hello@bmediax.dev>
2021-10-31 13:32:50 -0700

    Changes

--- src/layout/portfolioTemplate.js
+++ src/layout/portfolioTemplate.js
@@ -8,8 +8,6 @@ import NextProjectSection from "../components/NextProjectSection";
 import OverviewSection from "../components/OverviewSection";
 import PortfolioTitle from "../components/PortfolioTitle";
 import ResultsSection from "../components/ResultsSection";
-
-
 // import * as portfolioStyles from '../styles/portfolio.module.scss'
 
 const PortfolioTemplate = ({ data, pageTitle }) => {
