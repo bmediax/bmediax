246ffa306d43fd109be4de89617923123aa53e01
Bmediax <hello@bmediax.dev>
2022-04-11 19:46:40 -0700

    Remove Legion from Portfolio template

--- src/layout/PortfolioTemplate.js
+++ src/layout/PortfolioTemplate.js
@@ -3,7 +3,6 @@ import Layout from "../layout";
 import CoverTitle from "../components/CoverTitle";
 import DesignSection from "../components/DesignSection";
 import ImageSection from "../components/ImageSection";
-import LegionBar from "../components/modules/LegionBar";
 import NextProjectSection from "../components/NextProjectSection";
 import OverviewSection from "../components/OverviewSection";
 import PortfolioTitle from "../components/PortfolioTitle";
